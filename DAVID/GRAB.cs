31dbafc1d0286f596a755e76e0a4d7661465373c
hdilab-uta <61024755+hdilab-uta@users.noreply.github.com>
2020-02-13 12:25:12 -0600

    Add files via upload

---
+++ DAVID/GRAB.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GRAB : MonoBehaviour
+{
+    public Transform dest;
+
+    void OnMouseDown()
+    {
+        GetComponent<Rigidbody>().useGravity = false;
+        this.transform.position = dest.position;
+        this.transform.parent = GameObject.Find("guide").transform;
+        
+    }
+
+    void OnMouseUp()
+    {
+        GetComponent<Rigidbody>().useGravity = true;
+        this.transform.position = dest.position;
+        this.transform.parent = null;
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ DAVID/mouselook.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouselook : MonoBehaviour
+{
+    public float msen = 1000f;
+    public Transform playerBod;
+    float xrot = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * msen * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * msen * Time.deltaTime;
+
+        playerBod.Rotate(Vector3.up * mouseX);
+        xrot -= mouseY;
+        xrot = Mathf.Clamp(xrot, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(xrot, 0f, 0f);
+
+
+    }
+}
